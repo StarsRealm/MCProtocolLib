3e1e0437aba1bc86d907255a315baf9834ac9501
Simple <L834787411@outlook.com>
2024-06-18 00:53:07 +0800

    fix: fix merge error

--- protocol/build.gradle.kts
+++ protocol/build.gradle.kts
@@ -44,7 +44,8 @@ tasks.jacocoTestReport {
         xml.required = false
         csv.required = false
         html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
-
+    }
+}
 
 publishing {
     repositories {
